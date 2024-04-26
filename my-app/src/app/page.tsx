03a250523a9ed0ce755675c195e89a499f038ab5
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-04-26 21:30:15 +0300

    add mui

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@mui/material";
+import AbcIcon from "@mui/icons-material/Abc";
+
 export default function Home() {
 	return (
 		<main>
-			<div className="container">hi</div>
+			<div className="container">
+				<Button>test</Button>
+				<AbcIcon />
+			</div>
 		</main>
 	);
 }
