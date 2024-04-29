e9283a8765c7a47d84fe1fa2984252dbb4223065
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-04-29 23:39:54 +0300

    auth check

--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import CelebrationAnimation from "../components/CelebrationAnimation";
 import Navbar from "../components/Navbar";
+import { isLoggedIn } from "../services/authService";
+import { useRouter } from "next/navigation";
 
 const Products = () => {
+	const loggedIn = isLoggedIn();
+	const router = useRouter();
+	useEffect(() => {
+		if (!loggedIn) {
+			router.push("/login");
+		}
+	}, []);
 	return (
 		<div className="Products">
 			<CelebrationAnimation />
