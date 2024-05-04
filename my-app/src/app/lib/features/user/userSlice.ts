7d35a60653414da5cb9c3b738f33e0b70aeeac4f
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-05-04 19:58:46 +0300

    fix

--- my-app/src/app/lib/features/user/userSlice.ts
+++ my-app/src/app/lib/features/user/userSlice.ts
@@ -30,7 +30,8 @@ export interface User {
 }
 
 // Check if a token is stored in localStorage
-const storedToken = localStorage.getItem("token");
+const storedToken =
+	typeof window !== "undefined" ? localStorage.getItem("token") : null;
 
 // Define the initial state for user
 export const initialState: User = {
@@ -45,11 +46,15 @@ const userSlice = createSlice({
 	reducers: {
 		startSession: (state, action: PayloadAction<string>) => {
 			// Update the user state with the token
-			localStorage.setItem("token", action.payload);
+			if (typeof window !== "undefined") {
+				localStorage.setItem("token", action.payload);
+			}
 			state.token = action.payload;
 		},
 		endSession: (state) => {
-			localStorage.removeItem("token");
+			if (typeof window !== "undefined") {
+				localStorage.removeItem("token");
+			}
 			state.token = null;
 		},
 	},
