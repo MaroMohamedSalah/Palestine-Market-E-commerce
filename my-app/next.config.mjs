b8d54b7f858407c93e293c2156cc3802f4684573
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-05-07 19:03:03 +0300

    update links

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: [],
+		domains: [
+			"fakestoreapi.com",
+			"i0.wp.com",
+			"www.spirospathis.com",
+			"v7-benefits.com",
+			"m.media-amazon.com",
+			"edita.com.eg",
+			"th.bing.com",
+			"metro-website-images.s3.eu-west-1.amazonaws.com",
+			"cdnprod.mafretailproxy.com",
+			"i.pinimg.com",
+			"fayedshop.com",
+			"maxmartonline.com",
+			"ecart-eg.com",
+			"mcprod.hyperone.com.eg",
+			"greenhouseshopegypt.com",
+		],
 	},
 };
 
